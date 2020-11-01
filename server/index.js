80498b96d8ac63770c6dc48d664f39f867a05506
seanmcquaid <sean.mcquaid2@verizon.net>
2020-11-01 18:34:02 -0500

    removed env file and started the server

--- server/index.js
+++ server/index.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const cors = require('cors');
-const graphqlHTTP = require('express-graphql');
+const jwt = require('express-jwt');
+const typeDefs = require('./schemas/schema');
+const resolvers = require('./resolvers/resolvers');
+const JWT_SECRET = process.env.JWT_SECRET;
+console.log(JWT_SECRET);
 
 const app = express();
-
+const auth = jwt({
+  secret: JWT_SECRET,
+  credentialsRequired: false,
+});
+app.use(auth);
 app.use(cors());
 
-app.use('/graphql', graphqlHTTP());
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log('listening on port 3000');
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
 });
