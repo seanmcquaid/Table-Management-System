07e5f714badf4269be28eeced494aeeb9cd1a89a
seanmcquaid <sean.mcquaid2@verizon.net>
2020-11-01 20:52:42 -0500

    set up apollo client

--- client/src/main.js
+++ client/src/main.js
@@ -2,8 +2,11 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import { DefaultApolloClient } from '@vue/apollo-composable';
+import { apolloClient } from './apollo';
 
 createApp(App)
   .use(store)
   .use(router)
+  .provide(DefaultApolloClient, apolloClient)
   .mount('#app');
