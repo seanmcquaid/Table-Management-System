bc067cdf69c03040675196ffd5e9a14c98c0dc0c
seanmcquaid <sean.mcquaid2@verizon.net>
2020-11-04 23:07:28 -0500

    added in login mutation

--- client/src/apollo/mutations/index.js
+++ client/src/apollo/mutations/index.js
@@ -1,5 +1,11 @@
 import gql from 'graphql-tag';
 
-export const loginMutation = gql``;
+export const loginMutation = gql`
+  mutation LoginMutation($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      token
+    }
+  }
+`;
 
 export const registerMutation = gql``;
