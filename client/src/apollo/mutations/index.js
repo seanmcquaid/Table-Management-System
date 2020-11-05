62d2a65ef218ff510c3c5a69705aa4ebf0af8aff
seanmcquaid <sean.mcquaid2@verizon.net>
2020-11-04 23:15:09 -0500

    added register mutation

--- client/src/apollo/mutations/index.js
+++ client/src/apollo/mutations/index.js
@@ -2,10 +2,12 @@ import gql from 'graphql-tag';
 
 export const loginMutation = gql`
   mutation LoginMutation($username: String!, $password: String!) {
-    login(username: $username, password: $password) {
-      token
-    }
+    login(username: $username, password: $password)
   }
 `;
 
-export const registerMutation = gql``;
+export const registerMutation = gql`
+  mutation RegisterMutation($username: String!, $password: String!) {
+    register(username: $username, password: $password)
+  }
+`;
