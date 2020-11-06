b09b784b238c05368e7b2278ba8cd85b3f0b1b38
seanmcquaid <sean.mcquaid2@verizon.net>
2020-11-05 22:14:00 -0500

    started working on custom text input component, will finish tomorrow

---
+++ client/src/components/universal/TextInput.vue
@@ -0,0 +1,20 @@
+<template>
+  <input @input="onChange" />
+</template>
+
+<script>
+import {} from 'vue';
+export default {
+  setup(props, { emit }) {
+    const onChange = event => {
+      emit('input', event.target.value);
+    };
+
+    return {
+      onChange,
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ client/src/views/Login/Login.vue
@@ -0,0 +1,41 @@
+<template>
+  <PageLayout>
+    <H1>login</H1>
+    <TextInput v-model="username" />
+  </PageLayout>
+</template>
+
+<script>
+import { ref } from 'vue';
+import PageLayout from '@/layouts/PageLayout.vue';
+import H1 from '@/components/universal/Typography/H1.vue';
+import TextInput from '@/components/universal/TextInput.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    PageLayout,
+    H1,
+    TextInput,
+  },
+  setup() {
+    const username = ref('');
+    console.log(username);
+
+    return {
+      username,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.linkButtonsContainer {
+  height: 100px;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+}
+</style>
