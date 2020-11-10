36801727878af6b3493caec96ae439ff42e3da1a
seanmcquaid <sean.mcquaid2@verizon.net>
2020-11-10 17:57:20 -0500

    added in button to go to add table config

--- client/src/views/TableManagement/TableManagement.vue
+++ client/src/views/TableManagement/TableManagement.vue
@@ -1,16 +1,32 @@
 <template>
-  <PageLayout><H1>Table Management System</H1></PageLayout>
+  <PageLayout>
+    <H1>Table Management System</H1>
+    <div class="linkButtonsContainer">
+      <LinkButton route="/addTableConfiguration">
+        Add Table Configuration
+      </LinkButton>
+    </div>
+  </PageLayout>
 </template>
 
 <script>
 import PageLayout from '@/layouts/PageLayout.vue';
 import H1 from '@/components/universal/Typography/H1.vue';
+import LinkButton from '@/components/universal/LinkButton.vue';
 export default {
   components: {
     PageLayout,
     H1,
+    LinkButton,
   },
 };
 </script>
 
-<style></style>
+<style>
+.linkButtonsContainer {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
