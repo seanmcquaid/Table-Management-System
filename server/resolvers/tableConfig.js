9847d33824f728b8f17ae80ebe47da946ff9873a
seanmcquaid <sean.mcquaid2@verizon.net>
2020-11-11 22:12:11 -0500

    started sketching resolvers for table config

--- server/resolvers/tableConfig.js
+++ server/resolvers/tableConfig.js
@@ -1,6 +1,14 @@
 const resolvers = {
-  Query: {},
-  Mutation: {},
+  Query: {
+    getTableConfigInfo: (_, { id }, { token }) => {},
+    getAllTableConfigs: (_, args, { token }) => {},
+  },
+  Mutation: {
+    addTable: (_, { name, seats }, { token }) => {},
+    editTable: (_, { name, seats }, { token }) => {},
+    changeTableAvailability: (_, { name, isAvailable }, { token }) => {},
+    deleteTable: (_, { name }, { token }) => {},
+  },
 };
 
 module.exports = resolvers;
