d35c55651b12225355be31ff010e6fd93cd28dcf
seanmcquaid <sean.mcquaid2@verizon.net>
2020-11-11 21:41:36 -0500

    added in table config resolvers file

--- server/resolvers/index.js
+++ server/resolvers/index.js
@@ -1,7 +1,9 @@
 const userResolvers = require('./user');
+const tableConfigResolvers = require('./tableConfig');
 
 const resolvers = {
   ...userResolvers,
+  ...tableConfigResolvers,
 };
 
 module.exports = resolvers;
