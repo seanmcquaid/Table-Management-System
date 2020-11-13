a963f6f4eb4a30c69435b74d605fab102171fdd5
seanmcquaid <sean.mcquaid2@verizon.net>
2020-11-12 21:20:49 -0500

    structured out components

--- client/src/views/TableConfigurations/TableConfigurations.vue
+++ client/src/views/TableConfigurations/TableConfigurations.vue
@@ -1,16 +1,26 @@
 <template>
   <PageLayout>
     <H1>Table Configurations</H1>
+    <EditSeatingCapacityForm />
+    <AddTableForm />
+    <Tables />
   </PageLayout>
 </template>
 
 <script>
 import PageLayout from '@/layouts/PageLayout.vue';
 import H1 from '@/components/universal/Typography/H1.vue';
+import EditSeatingCapacityForm from './EditSeatingCapacityForm.vue';
+import AddTableForm from './AddTableForm.vue';
+import Tables from './Tables.vue';
+
 export default {
   components: {
     PageLayout,
     H1,
+    EditSeatingCapacityForm,
+    AddTableForm,
+    Tables,
   },
 };
 </script>
