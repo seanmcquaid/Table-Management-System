b834efb4f023278814664ccd095bf0031909febc
seanmcquaid <sean.mcquaid2@verizon.net>
2020-11-15 14:01:57 -0500

    wrote all mutations

--- client/src/apollo/mutations/index.js
+++ client/src/apollo/mutations/index.js
@@ -11,3 +11,27 @@ export const registerMutation = gql`
     register(username: $username, password: $password)
   }
 `;
+
+export const addTableMutation = gql`
+  mutation addTableMutation($name: String!, $seats: Int!) {
+    addTable(name: $name, seats: $seats)
+  }
+`;
+
+export const editTableMutation = gql`
+  mutation editTableMutation($id: Int!, $name: String!, $seats: Int!) {
+    editTable(id: $id, name: $name, seats: $seats)
+  }
+`;
+
+export const changeTableAvailabilityMutation = gql`
+  mutation changeTableAvailabilityMutation($id: Int!, $isAvailable: Boolean!) {
+    changeTableAvailability(id: $id, $isAvailable: $$isAvailable)
+  }
+`;
+
+export const deleteTableMutation = gql`
+  mutation deleteTableMutation($id: Int!) {
+    deleteTable(id: $id)
+  }
+`;
