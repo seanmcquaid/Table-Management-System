c4b5036c8b683257365ca4677b9ad4c8bd41d60c
seanmcquaid <sean.mcquaid2@verizon.net>
2020-11-14 23:08:13 -0500

    started sketching out actions and api service layers to be created

---
+++ client/src/api/tableService.js
@@ -0,0 +1,11 @@
+import { apolloClient } from '../apollo';
+
+export const getTableConfigInfo = () => {};
+
+export const getTableInfo = id => {};
+
+export const addTable = tableInfo => {};
+
+export const deleteTable = (id, isAvailable) => {};
+
+export const editTable = tableInfo => {};
--- client/src/store/modules/tableConfig/actions/index.js
+++ client/src/store/modules/tableConfig/actions/index.js
@@ -1,3 +1,9 @@
-const actions = {};
+const actions = {
+  getTableConfigInfoAction: ({ commit }, payload) => {},
+  addTableAction: ({ commit }, payload) => {},
+  editTableAction: ({ commit }, payload) => {},
+  changeTableAvailabilityAction: ({ commit }, payload) => {},
+  deleteTableAction: ({ commit }, payload) => {},
+};
 
 export default actions;
