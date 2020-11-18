3fec2b687c3d802ecda682d03ec79076e55f1382
seanmcquaid <sean.mcquaid2@verizon.net>
2020-11-17 22:36:55 -0500

    automated new resolver file additions so I don't need to update index each time

--- server/resolvers/index.js
+++ server/resolvers/index.js
@@ -1,9 +1,19 @@
-const userResolvers = require('./user');
-const tableResolvers = require('./table');
+const fs = require('fs');
+const path = require('path');
 
 const resolvers = {
-  Query: { ...userResolvers.Query, ...tableResolvers.Query },
-  Mutation: { ...userResolvers.Mutation, ...tableResolvers.Mutation },
+  Query: {},
+  Mutation: {},
 };
+
+const files = fs.readdirSync(path.join(__dirname, '/'));
+
+files.forEach((file) => {
+  if (file !== 'index.js') {
+    const resolver = require(`./${file}`);
+    resolvers.Query = { ...resolvers.Query, ...resolver.Query };
+    resolvers.Mutation = { ...resolvers.Mutation, ...resolver.Mutation };
+  }
+});
 
 module.exports = resolvers;
