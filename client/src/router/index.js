61a51f3936ba8dda85636b990a747b8f944bfc4e
seanmcquaid <sean.mcquaid2@verizon.net>
2020-11-17 22:08:19 -0500

    removed route guarding until I read more about it

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,7 +3,7 @@ import Home from '../views/Home/Home.vue';
 import Login from '../views/Login/Login.vue';
 import Register from '../views/Register/Register.vue';
 import protectedRoutes from './protectedRoutes';
-import store from '../store';
+// import store from '../store';
 
 const routes = [
   {
@@ -29,22 +29,23 @@ const router = createRouter({
   routes,
 });
 
-router.beforeEach((to, from, next) => {
-  const isProtectedRoute =
-    from.path !== '/home' &&
-    from.path !== '/login' &&
-    from.path !== '/register';
-  const isAuthenticatedAfterLoadingWithNoError =
-    !store.state.isLoading &&
-    store.state.auth.isAuthenticated &&
-    !store.state.auth.errorMessage;
-  if (isProtectedRoute && isAuthenticatedAfterLoadingWithNoError) {
-    next();
-  } else if (!isProtectedRoute) {
-    next();
-  } else {
-    next('/');
-  }
-});
+// need to revisit this later
+// router.beforeEach((to, from, next) => {
+//   const isProtectedRoute =
+//     from.path !== '/home' &&
+//     from.path !== '/login' &&
+//     from.path !== '/register';
+//   const isAuthenticatedAfterLoadingWithNoError =
+//     !store.state.isLoading &&
+//     store.state.auth.isAuthenticated &&
+//     !store.state.auth.errorMessage;
+//   if (isProtectedRoute && isAuthenticatedAfterLoadingWithNoError) {
+//     next();
+//   } else if (!isProtectedRoute) {
+//     next();
+//   } else {
+//     next('/');
+//   }
+// });
 
 export default router;
