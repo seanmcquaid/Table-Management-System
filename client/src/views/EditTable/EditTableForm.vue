e13219a1fd896f2c20ad503541e0225d7ec2b725
seanmcquaid <sean.mcquaid2@verizon.net>
2020-11-23 23:11:13 -0500

    set up basic structure for backend calls for edit table form

--- client/src/views/EditTable/EditTableForm.vue
+++ client/src/views/EditTable/EditTableForm.vue
@@ -3,7 +3,53 @@
 </template>
 
 <script>
-export default {};
+import * as tableService from '../../api/tableService';
+import { onMounted, reactive, toRefs } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import { useStore } from 'vuex';
+import useErrorMessage from '@/composables/useErrorMessage';
+
+export default {
+  setup() {
+    const store = useStore();
+    const { errorMessage } = useErrorMessage();
+    const state = reactive({ name: '', seats: 0 });
+    const router = useRouter();
+    const route = useRoute();
+    const { id } = route.params;
+
+    onMounted(() => {
+      tableService
+        .getTable(id)
+        .then(({ getTable: { name, seats } }) => {
+          state.name = name;
+          state.seats = seats;
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    });
+
+    const onSubmit = () => {
+      store
+        .dispatch('editTableAction', {
+          name: state.name,
+          seats: state.seats,
+          id,
+        })
+        .then(() => {
+          if (!errorMessage.value) {
+            router.push('/tableManagement');
+          }
+        });
+    };
+
+    return {
+      ...toRefs(state),
+      onSubmit,
+    };
+  },
+};
 </script>
 
 <style></style>
