b576965ee06fd1675e75fc59bdd5151a38158589
seanmcquaid <sean.mcquaid2@verizon.net>
2020-11-26 22:17:10 -0500

    started working on Tables component for current tables

--- client/src/views/CurrentTables/Tables.vue
+++ client/src/views/CurrentTables/Tables.vue
@@ -1,7 +1,54 @@
-<template> </template>
+<template>
+  <ul>
+    <li v-for="table in tables" :key="table.id">
+      <div class="tableInfo">
+        <span>{{ table.name }} </span>
+        <span>Seating Capacity : {{ table.seats }}</span>
+      </div>
+      <Button type="button" :disabled="!table.isAvailable">
+        Available
+      </Button>
+      <Button type="button" :disabled="table.isAvailable">
+        Occupied
+      </Button>
+    </li>
+  </ul>
+</template>
 
 <script>
-export default {};
+import Button from '@/components/Button.vue';
+import { computed } from 'vue';
+import { useStore } from 'vuex';
+
+export default {
+  components: { Button },
+  setup() {
+    const store = useStore();
+    const tables = computed(() => store.state.tableConfig.tables);
+
+    return {
+      tables,
+    };
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+ul {
+  list-style: none;
+}
+
+li {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+
+.tableInfo {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
